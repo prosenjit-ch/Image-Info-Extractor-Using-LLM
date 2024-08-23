fe625a18d7aa369d6711574006d26da4ee2183e5
Prosenjit Chakraborty <116121494+prosenjit-ch@users.noreply.github.com>
2024-08-24 01:13:29 +0600

    Invoice_Extractor.py

--- Invoice_Extractor.py
+++ Invoice_Extractor.py
@@ -11,7 +11,7 @@ genai.configure(api_key=os.environ.get('GOOGLE_API_KEY'))
 
 
 
-model = genai.GenerativeModel('gemini-pro-vision')
+model = genai.GenerativeModel('gemini-1.5-flash')
 
 def get_gemini_response(input, image, prompt):
     response = model.generate_content([input, image[0], prompt])
